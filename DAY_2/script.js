bbb76a5ce4083bb9e99cf83a202885fcaa491c9e
dilip9398 <138967561+dilip9398@users.noreply.github.com>
2026-09-17 21:47:25 +0530

    day-2

---
+++ DAY_2/script.js
@@ -0,0 +1,26 @@
+console.log("Hello")
+
+let a = 6;
+
+console.log(a);
+console.log(typeof(a));
+
+let b = "hello";
+console.log(b);
+console.log(typeof(b));
+
+const d = true;
+console.log(d);
+console.log(typeof(d));
+
+let object = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+console.log(object);
+console.log(typeof(object));
+console.log(object.name);
+
+const arr = [1, 2, 'afg'];
+console.log(arr);
